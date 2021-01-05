6fda2e7e1c26fe486147729287ac45c008f57c61
Lis-cyber <lisjardim6@gmail.com>
2021-01-04 23:11:55 -0300

    Comments

--- api/src/index.js
+++ api/src/index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const server = express();
-const bodyParser = require('body-parser');
 var cors = require('cors');
 
 const routes = require('./routes/index.js');
 // El Intercambio de Recursos de Origen Cruzado (CORS) es un mecanismo que utiliza cabeceras HTTP adicionales para permitir que un user agent obtenga permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
 // CORS, que es un mecanismo de seguridad del navegador que evita hacer requerimientos desde un dominio o dirección que no está permitida por el servidor.
 server.use(cors());
-server.use(express.json());
-server.use(bodyParser.json());
 server.use("/", routes);
 
 
 server.listen(1337, console.log("Server listening at port 1337"));
 
 module.exports = server;
+
+
+
+
+// body-parser es un middleware que sirve para parsear (darle formato) a las request que llegan por body
+// const bodyParser = require('body-parser');
+// server.use(express.json());
+// server.use(bodyParser.json());
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -6,61 +6,64 @@ const Cart = ({ cartItems, removeFromCart }) => {
   return (
     // Esta es la sintaxis para añadir un styled-component, reemplaza el <div>
     <CartStyled>
-      <div className="empty">
-        {/* Este operador condicional (ternario), se utiliza para evaluar el numero de productos de CartItems, por lo que, si la condición es verdadera, que indica que 
-        el carrito tiene 0 productos, muestra un <div> con un mensaje "Cart is empty", en cambio, cuando ya no cumple la condición y el carrito tiene un número mayor a 0,
-        muestra un <div> con un mensaje señalando la cantidad de productos que está en el carrito con la variable cartItems.length para la cantidad de productos.*/}
-        {cartItems.length === 0 ? (
-          <div>Cart is empty</div>
-        ) : (
-          <div>Your cart have {cartItems.length} different items !</div>
-        )}
-      </div>
-      <div>
-        {/* Este operador AND, lo que representa es que, cuando la cantidad de productos del carrito es distinta a 0, muestra el precio total de los productos*/}
-        <br />
-        {cartItems.length !== 0 && (
-          <div className="total">
-            Total: ${" "}
-            {/* Esta función, lo que hace es darle un formato al número, mostrando el . para los miles y , para los decimales*/}
-            {Intl.NumberFormat("de-DE").format(
-              cartItems.reduce((acc, curr) => acc + curr.price * curr.count, 0)
-            )}
-            {" ARS"}
-          </div>
-        )}
-        <br />
+      <div className="test">
+        <div className="empty">
+          {/* Este operador condicional (ternario), se utiliza para evaluar el numero de productos de CartItems, por lo que, si la condición es verdadera, que indica que 
+          el carrito tiene 0 productos, muestra un <div> con un mensaje "Cart is empty", en cambio, cuando ya no cumple la condición y el carrito tiene un número mayor a 0,
+          muestra un <div> con un mensaje señalando la cantidad de productos que está en el carrito con la variable cartItems.length para la cantidad de productos.*/}
+          {cartItems.length === 0 ? (
+            <div>Cart is empty</div>
+          ) : (
+            <div>Your cart have {cartItems.length} different items !</div>
+          )}
+        </div>
         <div>
-          <ul>
-            {/* Se definen los cartItem como números para listarlos */}
-            {/* Se realiza un map, en el array de cartItems, para mostrar uno por uno */}
-            {cartItems.map((item) => (
-              <li className="product-cart" key={item.id}>
-                <img src={item.thumbnail} alt={item.title}></img>
+          {/* Este operador AND, lo que representa es que, cuando la cantidad de productos del carrito es distinta a 0, muestra el precio total de los productos*/}
+          <br />
+          {cartItems.length !== 0 && (
+            <div className="total">
+              Total: ${" "}
+              {/* Esta función, lo que hace es darle un formato al número, mostrando el . para los miles y , para los decimales*/}
+              {Intl.NumberFormat("de-DE").format(
+                cartItems.reduce((acc, curr) => acc + curr.price * curr.count, 0)
+              )}
+              {" ARS"}
+            </div>
+          )}
+          <br />
+          {cartItems.length > 0 ? 
+          <div className="scroll">
+            <ul>
+              {/* Se definen los cartItem como números para listarlos */}
+              {/* Se realiza un map, en el array de cartItems, para mostrar uno por uno */}
+              {cartItems.map((item) => (
+                <li className="product-cart" key={item.id}>
+                  <img src={item.thumbnail} alt={item.title}></img>
 
-                <div className="infoDiv">
-                  <div className="info-text">
-                    <div>Quantity: {item.count}</div>
-                    <div>
-                      Price: $ {Intl.NumberFormat("de-DE").format(item.price)}{" "}
-                      {item.currency_id}
+                  <div className="infoDiv">
+                    <div className="info-text">
+                      <div>Quantity: {item.count}</div>
+                      <div>
+                        Price: $ {Intl.NumberFormat("de-DE").format(item.price)}{" "}
+                        {item.currency_id}
+                      </div>
+                      <div>
+                        Subtotal: ${" "}
+                        {Intl.NumberFormat("de-DE").format(
+                          item.price * item.count
+                        )}{" "}
+                        {item.currency_id}
+                      </div>
                     </div>
-                    <div>
-                      Subtotal: ${" "}
-                      {Intl.NumberFormat("de-DE").format(
-                        item.price * item.count
-                      )}{" "}
-                      {item.currency_id}
+                    <div className="button-remove">
+                      {/* Este es un boton que sirve para eliminar el producto del carrito, esta función es pasada por props. Al hacerle click, se invoca la función */}
+                      <button onClick={() => removeFromCart(item)}>Remove</button>
                     </div>
                   </div>
-                  <div className="button-remove">
-                    {/* Este es un boton que sirve para eliminar el producto del carrito, esta función es pasada por props. Al hacerle click, se invoca la función */}
-                    <button onClick={() => removeFromCart(item)}>Remove</button>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+          </div> : null }
         </div>
       </div>
     </CartStyled>
