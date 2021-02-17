8a2ba1b2f6fb57fc8feffbf402de1a57a6235ba2
Lis-cyber <lisjardim6@gmail.com>
2021-02-17 18:17:16 -0300

    Adding Procfile

--- api/src/index.js
+++ api/src/index.js
@@ -5,10 +5,7 @@ var cors = require('cors');
 const routes = require('./routes/index.js');
 server.use(cors());
 server.use("/", routes);
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  next()
-})
+
 const port = process.env.PORT || 3000
 server.listen( port , console.log(`Server listening at port ${port}`));
 
